dae1812d81095861b95ff3dd0bc412a0d8d9fa77
LydiaBesada <llydiaa@hotmail.com>
2023-10-06 13:35:46 +0200

    added background button

---
+++ main.js
@@ -0,0 +1,9 @@
+function clickFunction(){
+
+let ourBackground = document.querySelector("html")
+ourBackground.style.backgroundColor = " rgb(21, 150, 73)";
+}
+
+function getCurrentTimestamp () {
+    return Date.now()
+  }
