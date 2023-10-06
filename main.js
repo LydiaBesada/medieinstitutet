87bc724f5adfbcc93cb120e37c1f06beb96be2e1
j00w13 <johnjonsson89@gmail.com>
2023-10-06 14:35:46 +0200

    Added date/time stamp

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ let ourBackground = document.querySelector("html")
 ourBackground.style.backgroundColor = " rgb(21, 150, 73)";
 }
 
-function getCurrentTimestamp () {
-    return Date.now()
-  }
+function updateDateTime() {
+  // create a new `Date` object
+  const now = new Date();
+
+  // get the current date and time as a string
+  const currentDateTime = now.toLocaleString('sv-SE');
+
+  // update the `textContent` property of the `span` element with the `id` of `datetime`
+  document.querySelector('#datetime').textContent = currentDateTime;
+}
+
+// call the `updateDateTime` function every second
+setInterval(updateDateTime, 0);
