231cc0db0e0963d73414d2869fa86fa8acf95667
j00w13 <johnjonsson89@gmail.com>
2023-10-09 15:28:15 +0200

    We finished the tasks

--- main.js
+++ main.js
@@ -5,15 +5,71 @@ ourBackground.style.backgroundColor = " rgb(21, 150, 73)";
 }
 
 function updateDateTime() {
-  // create a new `Date` object
+  
+  setInterval(updateDateTime, 0);
   const now = new Date();
 
-  // get the current date and time as a string
+  
   const currentDateTime = now.toLocaleString('sv-SE');
 
-  // update the `textContent` property of the `span` element with the `id` of `datetime`
+  
   document.querySelector('#datetime').textContent = currentDateTime;
+
+  
 }
 
-// call the `updateDateTime` function every second
-setInterval(updateDateTime, 0);
+
+
+
+function calculator() {
+  let number1 = document.querySelector('#number1').value;  
+  let operator = document.querySelector('#operator').value; 
+  let number2 = document.querySelector('#number2').value;  
+  
+  let sum = 0
+
+  if (operator == '+') {
+    sum = parseInt(number1) + parseInt(number2);
+  } else if (operator == '-') {
+    sum = parseInt(number1) - parseInt(number2);
+  } else if (operator == '*') {
+    sum = parseInt(number1) * parseInt(number2);
+  } else if (operator == '/') {
+    sum = parseInt(number1) / parseInt(number2);
+  } else {
+    sum = 'Ange korrekta värden'
+  }
+  document.querySelector('#answer').textContent = sum
+ 
+}
+
+function validate(){
+  var name = document.getElementById("contact-name").value;
+  var phone = document.getElementById("contact-phone").value;
+  var email = document.getElementById("contact-email").value;
+  var message = document.getElementById("contact-comment").value;
+  var error_message = document.getElementById("error_message");
+  
+  error_message.style.padding = "10px";
+  
+  var text;
+  if(name.length < 5){
+    text = "Please Enter valid Name";
+    error_message.innerHTML = text;
+    return false;
+  }
+
+  if(isNaN(phone) || phone.length != 10){
+    text = "Please Enter valid Phone Number";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(email.indexOf("@") == -1 || email.length < 6){
+    text = "Please Enter valid Email";
+    error_message.innerHTML = text;
+    return false;
+  }
+
+  alert("Form Submitted Successfully!");
+  return true;
+}
